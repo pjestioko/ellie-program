63860390747ec2249f40876cf90090701d94c448
pjestioko <pauljoseph.estioko@gmail.com>
2021-06-15 20:13:19 -1000

    start navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Navbar() {
+    return (
+        <>
+            <div className="navbar">
+                <div className="navbar-container container">
+                    <Link to="/" className="navbar-logo">
+                        Kiala Elyse
+                    </Link>
+                    <div className="menu-icon">
+                        
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Navbar
